3c6b5340c9d38da7ce02fffab4ca96d77c8f4c24
rentsl <feng.ren@rivo-robot.com>
2017-04-17 15:17:53 +0800

    使用C

--- app/src/main/java/com/example/rentsl/moduletest/MainActivity.java
+++ app/src/main/java/com/example/rentsl/moduletest/MainActivity.java
@@ -3,11 +3,14 @@ package com.example.rentsl.moduletest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.rentsl.c.hello;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.out.println(hello.hello);
     }
 }
